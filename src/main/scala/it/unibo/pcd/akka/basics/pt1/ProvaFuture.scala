2906e5abf33fbe7b7773ec13890aff37a2fc083e
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-12-19 17:45:28 +0100

    loop with iterations and update

--- src/main/scala/it/unibo/pcd/akka/basics/pt1/ProvaFuture.scala
+++ src/main/scala/it/unibo/pcd/akka/basics/pt1/ProvaFuture.scala
@@ -22,14 +22,14 @@ import scala.util.{Random, Success}
  * posizione e collisione sui bordi
  *
  * POI
- * virtual tiem e GUIF
+ * virtual time e GUI
  */
 import Utils.computeTotalForceOnBody
 
 object ProvaFuture:
 
   enum API:
-    case Tick
+    case UpdateGUI
     case Stop
     case Restart
     case Msg(whom: String, to: ActorRef[Messaged])
@@ -37,13 +37,27 @@ object ProvaFuture:
 
   // TODO
   object Messager:
-    def apply(start: Int, end: Int, bodies: Seq[Body]): Behavior[API.Msg] = Behaviors.receive {
+
+    def apply(start: Int, end: Int, bodies: Seq[Body], bounds: Boundary, DT:Double): Behavior[API.Msg] = Behaviors.receive {
       case (ctx, API.Msg("stop", _)) =>
         ctx.log.info("ciaone")
         Behaviors.stopped
-      case (ctx, API.Msg("Forza", repl)) =>
-        val v = for i <- start until end yield computeTotalForceOnBody(bodies(i), bodies)
-        repl ! API.Messaged(v.toString(), ctx.self) // devo inviare ciò a chi aggiorna
+      case (ctx, API.Msg("Velocity", repl)) =>
+        for i <- start until end do
+          val b = bodies(i)
+          val totalForce = computeTotalForceOnBody(b, bodies)
+          /* compute instant acceleration */
+          val acc = V2d(totalForce).scalarMul(1.0 / b.mass)
+          /* update velocity */
+          b.updateVelocity(acc, DT)
+        repl ! API.Messaged("ho finito vel", ctx.self) // devo inviare ciò a chi aggiorna
+        Behaviors.same
+      case (ctx, API.Msg("Position", repl)) =>
+        for i <- start until end do
+          val b = bodies(i)
+          b.updatePos(DT)
+          b.checkAndSolveBoundaryCollision(bounds)
+        repl ! API.Messaged("ho finito pos", ctx.self)
         Behaviors.same
       case (ctx, msg) =>
         ctx.log.info("wow"+msg)
@@ -56,32 +70,54 @@ import Utils.createBodies
 object TestProvaFuture extends App:
   val N_ACTORS: Int = 4 //?
   val N_BODY: Int = 10
+  val N_ITERATIONS: Int = 10_000
 
   // TODO ACTOR SYSTEM
   val system: ActorSystem[API] = ActorSystem(
     Behaviors.setup[API] { ctx =>
-      Behaviors.withTimers { timers =>
+        val DT: Double = 0.001
+        var currentIteration: Int = 0
         val bounds: Boundary = Boundary(-50, -50, 50, 50)
         var actors: Seq[ActorRef[API.Msg]] = List.empty
         val bodies: Seq[Body] = createBodies(N_BODY, bounds)
-        for i <- 0 until N_ACTORS do actors = actors :+ ctx.spawn(Messager(bodies.size * i / N_ACTORS, bodies.size * (i + 1) / N_ACTORS, bodies), "printer" + i)
+        var arrivati: Int = 0
+        var vt: Double = 0
+
+        for i <- 0 until N_ACTORS do actors = actors :+ ctx.spawn(Messager(bodies.size * i / N_ACTORS, bodies.size * (i + 1) / N_ACTORS, bodies, bounds, DT), "printer" + i)
 
         Behaviors.receiveMessage{
           case API.Msg("Inizio", msg) =>
-            for a <- actors do a ! API.Msg("babbuota", ctx.self)
+            for a <- actors do a ! API.Msg("Velocity", ctx.self)
+            Behaviors.same
+          case API.Messaged("ho finito vel", msg) =>
+            arrivati = arrivati + 1
+            println("CALCOLO VEL "+arrivati)
+            if(arrivati == N_ACTORS)
+              arrivati = 0
+              for a <- actors do a ! API.Msg("Position", ctx.self)
+              println("ORA CALCOLATE POSIZIONE")
+            Behaviors.same
+          case API.Messaged("ho finito pos", msg) =>
+            arrivati = arrivati + 1
+            println("CALCOLO POS "+arrivati)
+            if(arrivati == N_ACTORS)
+              arrivati = 0
+              vt = vt + DT
+              println("AGGIORNO vt"+DT)
+              println("AGGIORNO vt"+vt)
+              println("NUOVO LOOP")
+              currentIteration = currentIteration + 1
+              if (currentIteration < N_ITERATIONS)
+                for a <- actors do a ! API.Msg("Velocity", ctx.self)
+              else Behaviors.stopped
+            // TODO deve dire di aggiornare la gui
+            // TODO UCCIDI I FIGLI PERCHè IL PROGRAMMA NON SE FERMA DA SOLO
             Behaviors.same
           case _ =>
             println("Bella")
             //for a <- actors do a ! Print("babbuota", ctx.self)
             Behaviors.same
-          // tutti gli attori devono calcolare la velocità sui loro body
 
-          // tutti gli attori devono calcolare la forza sui loro body
-
-          // tutti gli attori devono calcolare l'accelerazione sui loro body
-
-          //UPDATE VIEW
-        }
       }
     },
     name = "hello-world"
@@ -90,8 +126,7 @@ object TestProvaFuture extends App:
 
   system ! API.Msg("Inizio", system.ignoreRef)
   //Thread.sleep(1000)
-// TODO DOVE AGGIORNO LA ROBA :c
-// TODO I TIMER CI VANNO ?
+
 /**
  * Possiamo fare system ! dentro a receive
  */
