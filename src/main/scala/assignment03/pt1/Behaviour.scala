bece7cd03a8e12b2f3a63e06f553203da72e3c0b
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-12-27 09:41:59 +0100

    refactoring

--- src/main/scala/assignment03/pt1/Behaviour.scala
+++ src/main/scala/assignment03/pt1/Behaviour.scala
@@ -6,11 +6,15 @@ import assignment03.pt1.Body.Body
 import assignment03.pt1.ProvaBehaviour.API
 
 object Behaviour:
+  case class IterationData(DT: Double, arrived: Int = 0, vt: Double = 0, currentIteration: Int = 0)
+  
+
+  def updateBodies(bodies: Seq[Body], bodiesToUpdate: Seq[Body], start: Int, end: Int): Seq[Body] =
+    bodies map {b => if b.id >= start && b.id < end then bodiesToUpdate.filter(body => body.id == b.id).head else b}
+
   def behaviourReceive(bounds: Boundary, actors: Seq[ActorRef[API.Msg]], bodiesB:Seq[Body], viewer: ActorRef[API.UpdateGUI], N_ACTORS: Int, N_ITERATIONS: Int, ctx: ActorContext[API]): Behaviors.Receive[API] =
-    var arrivati: Int = 0
-    var vt: Double = 0
-    var currentIteration: Int = 0
-    val DT: Double = 0.001
+    val DT = 0.001
+    var data = IterationData(DT)
     println(bodiesB)
     println(" ")
     var bodies = Seq.from(bodiesB)
@@ -20,23 +24,19 @@ object Behaviour:
         Behaviors.same
       case API.Messaged("Velocity", start, end, bodiesToUpdate, msg) =>
         // TODO HO FINITO VEL
-        bodies = bodies map {b => if b.id >= start && b.id < end then bodiesToUpdate.filter(body => body.id == b.id).head else b} // TODO ???
-        arrivati = arrivati + 1
-        if(arrivati == N_ACTORS)
-          arrivati = 0
+        bodies = updateBodies(bodies, bodiesToUpdate, start, end) // TODO ???
+        data = data.copy(DT = DT, arrived = data.arrived + 1)
+        if(data.arrived == N_ACTORS)
+          data = data.copy(DT = DT, arrived = 0)
           for a <- actors do a ! API.Msg("Position", bodies.toList, ctx.self)
         Behaviors.same
       case API.Messaged("Position", start, end, bodiesToUpdate, msg) =>
-        bodies = bodies map {b => if b.id >= start && b.id < end then bodiesToUpdate.filter(body => body.id == b.id).head else b}
-        arrivati = arrivati + 1
-        if(arrivati == N_ACTORS)
-          arrivati = 0
-          vt = vt + DT
-          currentIteration = currentIteration + 1
-          if (currentIteration < N_ITERATIONS)
-          //println(currentIteration)
-            viewer ! API.UpdateGUI(vt, currentIteration, bodies.toList, bounds, ctx.self)
-          //for a <- actors do a ! API.Msg("Velocity", bodies.toList, ctx.self) // TODO ???
+        bodies = updateBodies(bodies, bodiesToUpdate, start, end)
+        data = data.copy(DT = DT, arrived = data.arrived + 1)
+        if(data.arrived == N_ACTORS)
+          data = data.copy(DT = DT, arrived = 0, vt = data.vt + data.DT, currentIteration = data.currentIteration + 1)
+          if (data.currentIteration < N_ITERATIONS)
+            viewer ! API.UpdateGUI(data.vt, data.currentIteration, bodies.toList, bounds, ctx.self)
           else Behaviors.stopped
         // TODO UCCIDI I FIGLI PERCHè IL PROGRAMMA NON SE FERMA DA SOLO
         Behaviors.same
@@ -45,6 +45,7 @@ object Behaviour:
           Behaviors.receiveMessage{
             case API.Start() =>
               // TODO START NON RIPRENDE A CAPO
+              println("start")
               for a <- actors do a ! API.Msg("Velocity", bodiesB, ctx.self)
               behaviourReceive(bounds, actors, bodiesB, viewer, N_ACTORS, N_ITERATIONS, ctx)
             case _ => Behaviors.same
